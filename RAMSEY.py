c8f858b9463ffe235beaa5154019e496c67cad26
Elijah <115517217+Alson71@users.noreply.github.com>
2023-10-27 11:52:54 -0400

    Update RAMSEY.py

--- RAMSEY.py
+++ RAMSEY.py
@@ -3,8 +3,9 @@ import customtkinter
 
 
 class RAMSEY:
-    global tkWindow,boroughs,boroughDropdown,cuisineDropdown,boroughSpecificMenu 
+    global tkWindow,parameters,dropdowns
     global cuisines,cuisines2,brooklynAreas,manhattanAreas,queensAreas,bronxAreas,statenAreas
+    global labels, options
     
     customtkinter.set_appearance_mode("dark")
     tkWindow = customtkinter.CTk()
@@ -12,19 +13,30 @@ class RAMSEY:
     tkWindow.title("RAMSEY")
     tkWindow.resizable("False","False")
     
-    
-    boroughs = ["Manhattan","Queens","Staten Island","Bronx","Brooklyn"]
-    cuisines = ["American","Indian","Chinese","Japanese","Greek","Mexican","French","Peruvian","Korean","Thai"]
-    
-    
-    
-    
-    var = StringVar()
-    var.set("Borough")
-    
-    boroughDropdown = customtkinter.CTkOptionMenu(tkWindow,variable = var,values = boroughs,height = 50, width = 100)
-    boroughDropdown.place(x = 430,y = 100)
+    parameters = [None] * 3
+    parameters[0] = ["American","Indian","Chinese","Japanese","Greek","Mexican","French","Peruvian","Korean","Thai""Cuban", "Dominican", "Vietnamese", "Spain", "Brazilian", "Colombian", "Filippino", "Jamaican", "Russian", "Italian"]
+    parameters[1] = ["Manhattan","Queens","Staten Island","Bronx","Brooklyn"]
+    parameters[2] = ["None","None","None","None","None"]
+    options = ["Cuisine", "Borough", "Area"]
     
     
+   
+    vars = [None] * 3
+    labels = [None] * 3
+    dropdowns = [None] * 3
+    increment = 230
+    for i in range(3):
+        vars[i] = StringVar()
+        vars[i].set("Select")
+        
+        labels[i] = customtkinter.CTkLabel(tkWindow,fg_color = "transparent", height = 30, width = 100,text = options[i])
+        dropdowns[i] = customtkinter.CTkOptionMenu(tkWindow,variable = vars[i],values = parameters[i],height = 30, width = 100)
+        
+        labels[i].place(x = increment, y = 75)
+        dropdowns[i].place(x = increment, y = 100)
+        increment += 200
+        
+   
 if __name__ == "__main__":
     tkWindow.mainloop()
+    
