768b38c03227cf02d05d08809563ae92c5936a32
Elijah <115517217+Alson71@users.noreply.github.com>
2023-11-25 00:42:35 -0500

    fixed the entire class

--- RAMSEY.py
+++ RAMSEY.py
@@ -4,121 +4,123 @@ from PIL import ImageTk
 import customtkinter
 import requests
 
-
-class RAMSEY:
-    global tkWindow,parameters,dropdowns
-    global cuisines,cuisines2,areas
-    global labels, options, vars, temp
-    
-    #To keep track of the option for area in "enableArea" method
-    temp = ""
-    
-    
-    #Areas for each borough
-    areas = [None] * 5
-    areas[0] = ["Upper East Side", "Upper West Side", "Midtown", "Greenwich", "East Village"]
-    areas[1] = ["Long Island City", "Astoria", "Forest Hills", "Queens Village", "Kew Gardens"]
-    areas[2] = ["Riverdale", "Morris Park", "Parkchester", "Pelham Bay", "Fordham"]
-    areas[3] = ["Brooklyn Heights", "DUMBO", "Prospect Heights", "Coney Island", "Flatbush"]
-    areas[4] = ["West Brighton", "St. George", "Fort Wadsworth", "Charleston", "Dongan Hills"]
-    #End
-    
-    #Initializing all of the Tkinter Windows
+#The first window
+class RAMSEYFrame1(customtkinter.CTk):
     customtkinter.set_appearance_mode("dark")
-    tkWindow = [None] * 3
-    for i in range(3):
-        tkWindow[i] = customtkinter.CTk()
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         w = 1000
         h = 600
-        screenWidth = tkWindow[i].winfo_screenwidth()
-        screenHeight = tkWindow[i].winfo_screenheight()
+        screenWidth = self.winfo_screenwidth()
+        screenHeight = self.winfo_screenheight()
         x = (screenWidth/2) - (w/2)
         y = (screenHeight/2) - (h/2)
     
-        tkWindow[i].geometry("%dx%d+%d+%d" % (w,h,x,y))
-        tkWindow[i].title("RAMSEY")
-        tkWindow[i].resizable("False","False")
-        tkWindow[i].wm_protocol("WM_DELETE_WINDOW",exit)
-    
-    parameters = [None] * 3
-    parameters[0] = ["American","Indian","Chinese","Japanese","Greek","Mexican","French","Peruvian","Korean","Thai","Cuban", "Dominican", "Vietnamese", "Spain", "Brazilian", "Colombian", "Filippino", "Jamaican", "Russian", "Italian"]
-    parameters[1] = ["Manhattan","Queens","Bronx","Brooklyn","Staten Island"]
-    parameters[2] = ["None"]
-    options = ["Cuisine", "Borough", "Area"]
-    #End
-    
-    #Tkinter Window 1
-    def closeWindow(*args):
-        tkWindow[0].destroy()
-        tkWindow[1].mainloop()
+        self.geometry("%dx%d+%d+%d" % (w,h,x,y))
+        self.title("RAMSEY")
+        self.resizable("False","False")
+        self.wm_protocol("WM_DELETE_WINDOW",exit)
         
-    #Sets the background image
-    background_image = PIL.Image.open(requests.get("https://www.bhmpics.com/downloads/food-background-tumblr/5.75dffe608c86bc7c9e2dd1467e6c266e.jpg", stream=True).raw)
-    width, height = tkWindow[0].winfo_screenwidth(), tkWindow[0].winfo_screenheight()
-    newImage1 = background_image.resize((width, height))
-    resizeImage1 = ImageTk.PhotoImage(newImage1)
-    background_label = customtkinter.CTkLabel(tkWindow[0], image=resizeImage1)
-    background_label.place(x= 0,y= 0)
+        background_image = PIL.Image.open(requests.get("https://www.bhmpics.com/downloads/food-background-tumblr/5.75dffe608c86bc7c9e2dd1467e6c266e.jpg", stream=True).raw)
+        newImage1 = background_image.resize((screenWidth, screenHeight))
+        resizeImage1 = ImageTk.PhotoImage(newImage1)
+        background_label = customtkinter.CTkLabel(self, image=resizeImage1)
+        background_label.place(x= 0,y= 0)
+        
+        anime_picture = PIL.Image.open(requests.get('https://64.media.tumblr.com/b670dd891997869721a4b2b1564aa6eb/tumblr_pklksruLv61u7vq8co1_r1_1280.png', stream =True).raw)
+        resizeImage2 = customtkinter.CTkImage(dark_image = anime_picture, size = (350,350))
+        picture = customtkinter.CTkLabel(self, image = resizeImage2, text = "")
+        picture.place(x = 330, y = 25)
+        
+        ramseyLabel = customtkinter.CTkLabel(self, text = "R.A.M.S.E.Y.", font = ('Script',60), height = 50, width = 110, text_color="white", fg_color='black')
+        otherRamseyLabel = customtkinter.CTkLabel(self, text = "(Remarkable Aromas Make Sizzling Enjoyable Yumminess)", font = ('Script',40), height = 50, width = 120, text_color="white", fg_color='black')
+        ramseyLabel.place(x = 380, y = 390)
+        otherRamseyLabel.place(x = 190, y = 450)
     
-    anime_picture = PIL.Image.open(requests.get('https://64.media.tumblr.com/b670dd891997869721a4b2b1564aa6eb/tumblr_pklksruLv61u7vq8co1_r1_1280.png', stream =True).raw)
-    resizeImage2 = customtkinter.CTkImage(dark_image = anime_picture, size = (350,350))
-    picture = customtkinter.CTkLabel(tkWindow[0], image = resizeImage2, text = "")
-    picture.place(x = 330, y = 25)
+        self.button = customtkinter.CTkButton(self, height = 70, width = 150, command = self.closeWindow, text = "Ready to Eat?")
+        self.button.place(x = 420, y = 510)
+        
     
-    #The Ready to Eat Button
-    button = customtkinter.CTkButton(tkWindow[0], height = 70, width = 150, command = closeWindow, text = "Ready to Eat?")
-    button.place(x = 420, y = 510)
+        
+    def closeWindow(self):
+        self.destroy()
+        ramseyFrame2 = RAMSEYFrame2()
+        ramseyFrame2.mainloop()
+        
+#The second window   
+class RAMSEYFrame2(customtkinter.CTk):
     
-    #Title of the first page
-    ramseyLabel = customtkinter.CTkLabel(tkWindow[0], text = "R.A.M.S.E.Y.", font = ('Script',60), height = 50, width = 110, text_color="white", fg_color='black')
-    otherRamseyLabel = customtkinter.CTkLabel(tkWindow[0], text = "(Remarkable Aromas Make Sizzling Enjoyable Yumminess)", font = ('Script',40), height = 50, width = 120, text_color="white", fg_color='black')
-    ramseyLabel.place(x = 380, y = 390)
-    otherRamseyLabel.place(x = 190, y = 450)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)   
+        
+        w = 1000
+        h = 600
+        screenWidth = self.winfo_screenwidth()
+        screenHeight = self.winfo_screenheight()
+        x = (screenWidth/2) - (w/2)
+        y = (screenHeight/2) - (h/2)
     
+        self.geometry("%dx%d+%d+%d" % (w,h,x,y))
+        self.title("RAMSEY")
+        self.resizable("False","False")
+        self.wm_protocol("WM_DELETE_WINDOW",exit)
+        
+        background_image = PIL.Image.open(requests.get("https://www.bhmpics.com/downloads/food-background-tumblr/5.75dffe608c86bc7c9e2dd1467e6c266e.jpg", stream=True).raw)
+        newImage1 = background_image.resize((screenWidth, screenHeight))
+        resizeImage1 = ImageTk.PhotoImage(newImage1)
+        background_label = customtkinter.CTkLabel(self, image=resizeImage1)
+        background_label.place(x= 0,y= 0)
+    
+        self.areas = [None] * 5
+        self.areas[0] = ["Upper East Side", "Upper West Side", "Midtown", "Greenwich", "East Village"]
+        self.areas[1] = ["Long Island City", "Astoria", "Forest Hills", "Queens Village", "Kew Gardens"]
+        self.areas[2] = ["Riverdale", "Morris Park", "Parkchester", "Pelham Bay", "Fordham"]
+        self.areas[3] = ["Brooklyn Heights", "DUMBO", "Prospect Heights", "Coney Island", "Flatbush"]
+        self.areas[4] = ["West Brighton", "St. George", "Fort Wadsworth", "Charleston", "Dongan Hills"]
 
-    #End
-    
-    #Tkinter Window 2     
-    vars = [None] * 3
-    labels = [None] * 3
-    dropdowns = [None] * 3
-    increment = 230
-    
-    for i in range(3):
-        vars[i] = StringVar()
-        vars[i].set("Select")
+        self.parameters = [None] * 3
+        self.parameters[0] = ["American","Indian","Chinese","Japanese","Greek","Mexican","French","Peruvian","Korean","Thai","Cuban", "Dominican", "Vietnamese", "Spain", "Brazilian", "Colombian", "Filippino", "Jamaican", "Russian", "Italian"]
+        self.parameters[1] = ["Manhattan","Queens","Bronx","Brooklyn","Staten Island"]
+        self.parameters[2] = ["None"]
         
-        labels[i] = customtkinter.CTkLabel(tkWindow[1],fg_color = "transparent", height = 30, width = 100,text = options[i])
-        dropdowns[i] = customtkinter.CTkOptionMenu(tkWindow[1],variable = vars[i],values = parameters[i],height = 30, width = 100)
-        
-        labels[i].place(x = increment, y = 75)
-        dropdowns[i].place(x = increment, y = 100)
-        increment += 200
-    
-    def enableArea(*args):
-        global vars, temp
+        self.options = ["Cuisine", "Borough", "Area"]
 
-        if temp != dropdowns[1].get() and dropdowns[1].get() != "Select" and dropdowns[2].cget("state") == "normal":
-            vars[2].set("Select")
-            
-        if dropdowns[0].get() != "Select" and dropdowns[1].get() != "Select":    
+        self.temp = ""
+    
+        self.vars = [None] * 3
+        self.labels = [None] * 3
+        self.dropdowns = [None] * 3
+        self.increment = 230
+    
+        for i in range(3):
+            self.vars[i] = StringVar()
+            self.vars[i].set("Select")
+        
+            self.labels[i] = customtkinter.CTkLabel(self,fg_color = "transparent", height = 30, width = 100,text = self.options[i])
+            self.dropdowns[i] = customtkinter.CTkOptionMenu(self,variable = self.vars[i],values = self.parameters[i],height = 30, width = 100)
+        
+            self.labels[i].place(x = self.increment, y = 75)
+            self.dropdowns[i].place(x = self.increment, y = 100)
+            self.increment += 200
+
+        self.vars[0].trace("w",self.enableArea)       
+        self.vars[1].trace("w", self.enableArea)
+        self.dropdowns[2].configure(state = "disabled")
+        
+    def enableArea(self, *args):
+        if self.temp != self.dropdowns[1].get() and self.dropdowns[1].get() != "Select" and self.dropdowns[2].cget("state") == "normal":
+                self.vars[2].set("Select")
+                  
+        if self.dropdowns[0].get() != "Select" and self.dropdowns[1].get() != "Select":    
             for i in range(5):
-                if dropdowns[1].get() == parameters[1][i]:
-                    dropdowns[2].configure(values = areas[i])
-                    temp = parameters[1][i]
-            dropdowns[2].configure(state = "normal")    
+                if self.dropdowns[1].get() == self.parameters[1][i]:
+                    self.dropdowns[2].configure(values = self.areas[i])
+                    self.temp = self.parameters[1][i]
+            self.dropdowns[2].configure(state = "normal")    
             
-        
-    
-    vars[0].trace("w",enableArea)       
-    vars[1].trace("w", enableArea)
-    
-    
-    dropdowns[2].configure(state = "disabled")
-    #End
-   
-    
-    
+      
    
 if __name__ == "__main__":
-   tkWindow[0].mainloop()
+   ramseyFrame1 = RAMSEYFrame1()
+   ramseyFrame1.mainloop()
